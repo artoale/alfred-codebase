f2da38fa6feef839ad2b2b1591a5a7f3ad545659
aartoni <aartoni@google.com>
2015-01-27 14:34:32 +0000

    Add test source to grunt test-ci

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
                 options: {
                     reporter: 'dot',
                 },
+                src: ['test/**/*.spec.js']
             }
         }
     });
