6cfb7f1fad5ee1a7f74763f131080add794792bd
aartoni <aartoni@google.com>
2015-01-27 22:18:49 +0000

    Add grunt-notify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,29 @@ module.exports = function(grunt) {
     // Add the grunt-mocha-test tasks.
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-notify');
 
     grunt.initConfig({
         // Configure a mochaTest task
         watch: {
             test: {
                 files: ['**/*.js'],
-                tasks: ['mochaTest:test'],
+                tasks: ['mochaTest:test', 'notify:test'],
+            }
+        },
+        notify_hooks: {
+            options: {
+                enabled: true,
+                // success: true, // whether successful grunt executions should be notified automatically
+                duration: 3 // the duration of notification in seconds, for `notify-send only
+            },
+        },
+        notify: {
+            test: {
+                options: {
+                    title: 'Mocha',
+                    message: 'All test passed'
+                }
             }
         },
         mochaTest: {
@@ -31,5 +47,6 @@ module.exports = function(grunt) {
 
     grunt.registerTask('default', 'watch');
     grunt.registerTask('test-ci', 'mochaTest:ci');
+    grunt.task.run('notify_hooks');
 
 };
