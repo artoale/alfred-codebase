e084b86e48760ef5f03b6fc7c7097b8685759090
aartoni <aartoni@google.com>
2015-01-28 18:19:08 +0000

    Add commands switch object, handle login param

--- command.js
+++ command.js
@@ -2,9 +2,27 @@
 'use strict';
 
 var query = process.argv[2].trim();
+var tokens, command;
+
+var commands = {
+    login: function (arg) {
+        if (arg) {
+            console.log('performing login:', arg);
+        } else {
+            console.log('Starting server...');
+        }
+    }
+};
 
 if (query[0] === '>') {
-	console.log('executing command...', query.slice(0));
+    query = query.replace('>', '').trim();
+    tokens = query.split(/\s/);
+    command = commands[tokens[0]];
+    if (typeof command === 'function') {
+        command(tokens[1]);
+    } else {
+	   console.log('executing command...', query.slice(0));
+    }
 } else {
 	console.log('opening ', process.argv[2]);
 }
