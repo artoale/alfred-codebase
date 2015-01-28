58c71a9fab6007e45048f508bbd4db6b35472b8f
aartoni <aartoni@google.com>
2015-01-28 16:05:45 +0000

    Add settings module

---
+++ src/settings.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function (fs, fileName) {
+    var _onError = function (error) {
+        if (error.code === 'ENOENT') { //file not found
+            return fs.write(fileName, '{}').then(function () {
+                return;
+            });
+        }
+
+        throw error;
+    };
+
+    var get = function (key) {
+        return fs.read(fileName).then(function (data) {
+            if (key) {
+                return JSON.parse(data)[key];
+            }
+            return JSON.parse(data);
+        }, _onError);
+    };
+
+    var set = function (key, value) {
+        return fs.read(fileName).catch(_onError).then(function (data) {
+            data = data;
+            var obj = data ? JSON.parse(data) : {};
+            obj[key] = value;
+            return fs.write(fileName, JSON.stringify(obj));
+        }).then(function (data) {
+            console.log('write', data);
+        });
+    };
+
+    return {
+        get: get,
+        set: set
+    };
+};
+
